8a1304eb200904e5ba350f73120cb441016b7208
francis-osei <oseifrancis673@gmail.com>
2025-05-30 07:14:13 -0700

    add Testimonials section
    - add card component
    - install tailwind-merge
    - install clsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import About from "./components/About";
 import Footer from "./components/footer";
 import { Textarea } from "./components/Textarea";
+import  Testimonials from './components/Testimonials'
 
 function App() {
   return (
     <>
-      {/* <h1 class="text-3xl font-bold underline">Hello world!</h1> */}
       <About />
+      <Testimonials />
       <Footer />
     </>
   );
