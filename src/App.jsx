339645758c3f7e74948265531d5fd3fa333edc43
francis-osei <oseifrancis673@gmail.com>
2025-05-30 10:17:08 -0700

    add contact info section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import About from "./components/About";
 import Footer from "./components/footer";
 import { Textarea } from "./components/Textarea";
-import  Testimonials from './components/Testimonials'
+import Testimonials from "./components/Testimonials";
+import ContactInfo from "./components/ContactInfo";
 
 function App() {
   return (
     <>
       <About />
       <Testimonials />
+      <ContactInfo />
       <Footer />
     </>
   );
