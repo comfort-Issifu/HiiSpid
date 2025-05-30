31e9c7842bf9321991fa34af76971292062baeef
francis-osei <oseifrancis673@gmail.com>
2025-05-30 14:33:06 -0700

    add featured dishes section
    - add button component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Footer from "./components/footer";
 import { Textarea } from "./components/Textarea";
 import Testimonials from "./components/Testimonials";
 import ContactInfo from "./components/ContactInfo";
+import { Dishes } from "./components/Dishes";
 
 function App() {
   return (
     <>
       <About />
       <Testimonials />
+      <Dishes />
       <ContactInfo />
       <Footer />
     </>
